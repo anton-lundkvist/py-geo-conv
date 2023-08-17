56edf9767b7584d6116f0d09a1b21f61643a4eb0
Anton Lundkvist <anton.lundkvist@skogsstyrelsen.se>
2023-08-17 15:21:52 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cf-remote",
+    version="1.0",
+    author="Anton Lundkvist",
+    author_email="antonlundkvist@gmail.com",
+    description="Pure python tool for converting geometries between WKT, GeoJson and Esri Json formats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anton-lundkvist/py-geo-conv",
+    packages=setuptools.find_packages(where="pygeoconv"),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License'",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+)
