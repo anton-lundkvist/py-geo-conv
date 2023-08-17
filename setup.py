aa4535b503a943036cedd02e00eed739269ad2ef
Anton Lundkvist <anton.lundkvist@skogsstyrelsen.se>
2023-08-17 15:22:32 +0200

    setup fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cf-remote",
+    name="pygeoconv",
     version="1.0",
     author="Anton Lundkvist",
     author_email="antonlundkvist@gmail.com",
