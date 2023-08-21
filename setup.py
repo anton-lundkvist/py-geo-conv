d1d2e82583380e6fd684544d730876e0efd3784f
Anton Lundkvist <anton.lundkvist@skogsstyrelsen.se>
2023-08-21 11:35:54 +0200

    publish step

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License'",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
